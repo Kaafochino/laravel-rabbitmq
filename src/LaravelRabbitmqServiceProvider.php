585e47c088c77122636b7d0a26dd46084e2719b1
k.pazoki <k.pazoki@dorehgard.ir>
2022-06-27 17:14:22 +0430

    initial package

---
+++ src/LaravelRabbitmqServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kaafochino\LaravelRabbitmq;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRabbitmqServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
